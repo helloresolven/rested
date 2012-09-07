c787eee7565718796a61beff03cfbf4249da75f2
Patrick <helloresolven@gmail.com>
2012-09-07 10:26:02 -0400

    restructure. improved.

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ It also includes all the test requests and servers used to make sure RESTed work
 ## Issues
 
 1. Since OAuth tokens are stored in local Core Data store, they won't work.
+2. You'll need your own TSL certificates for the https test server. (server.crt & server.key)
---
+++ servers/http.js
@@ -0,0 +1,57 @@
+var http = require("http"),
+    url = require("url"),
+    fs = require("fs");
+
+var server = http.createServer(function (req, res) {
+  req.setEncoding("utf8");
+  
+  var buffer = ''
+  req.on("data", function(data) {
+    buffer += data;
+  });
+  
+  req.on("end", function() {
+    console.log(new Date)
+    var parsedURL = url.parse(req.url, true);
+    
+    var responseHeaders = {};
+    
+    if(req.headers.cookie) {
+      console.log("Has a cookie: %j", req.headers.cookie);
+    } else {
+      responseHeaders["Set-Cookie"] = "uid=124";
+    }
+    
+    if(req.headers['content-type'] === 'application/json' && buffer.length > 0) {
+      console.log("Parsed HTTP Body: " + JSON.parse(buffer));
+    }
+    
+    if(req.headers['accept'] === 'text/html') {
+      res.writeHead(200, responseHeaders);
+      
+      fs.readFile(__dirname + '/test.html', function (err, data) {
+          if (err) {
+              res.statusCode = 500;
+              res.end(String(err));
+          } else  {
+            res.end(data);
+          }
+      });
+    } else {
+      responseHeaders["Content-Type"] = "application/json";
+      
+      var obj = {
+        method:req.method,
+        path:parsedURL.pathname,
+        query:parsedURL.query,
+        headers:req.headers,
+        body:buffer
+      };
+      
+      res.writeHead(200, responseHeaders);
+      res.end(JSON.stringify(obj));
+    }
+  });
+});
+
+server.listen(3000);
